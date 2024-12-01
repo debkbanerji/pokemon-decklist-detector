efff76b8954c2dc3d29555004be850cf9f43bdc4
debkbanerji <debkbanerji@gmail.com>
2024-12-01 00:04:47 -0800

    Card Loading Shimmer

--- client/src/CardImageForID.tsx
+++ client/src/CardImageForID.tsx
@@ -1,7 +1,24 @@
-const API_ADDRESS = 'http://' + location.hostname // TODO: Change before prod
+import { useEffect, useState } from 'react';
 
 function CardImageForID({ id }) {
-  return <img src={`/cards/${id}.png`} style={{width: '100%'}}/>;
+  const imageUrl = `/cards/${id}.png`;
+  const [hasLoaded, setHasLoaded] = useState(false);
+  useEffect(() => {
+    async function waitForLoad() {
+      if (!hasLoaded) {
+        await fetch(imageUrl); // wait for this network request to finish
+        setHasLoaded(true);
+      }
+    }
+    waitForLoad();
+  }, [hasLoaded, setHasLoaded,]);
+
+  return hasLoaded ?
+    <img src={imageUrl} style={{ width: '100%' }} /> :
+    <div className="card-image-loading-spinner-container">
+      <img src='/cardback.jpg' style={{ width: '100%' }}></img>
+      <span className="card-image-loading-spinner"></span>
+    </div>;
 }
 
 export default CardImageForID;
